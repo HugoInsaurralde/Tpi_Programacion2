d108a27a3330fdf10dfc9c31cf04a46a933563d8
neurobitsproducer <neurosisproduce@gmail.com>
2026-06-22 22:44:27 -0300

    Update Factura entity for 1-to-1 relationship
    se actualiza la calse fatura para respetar la relacion 1 a 1

--- TPI_Programacion/src/main/java/Entities/Factura.java
+++ TPI_Programacion/src/main/java/Entities/Factura.java
@@ -1,6 +1,6 @@
 /*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ * se actualizo la entidad factura para repetar la relaciuon 1 a 1 
+ * 
  */
 package Entities;
 
